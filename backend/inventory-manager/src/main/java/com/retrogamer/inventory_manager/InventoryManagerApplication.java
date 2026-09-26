a15bfce6cee9472d6ee73c4aa745b112b44d8841
Gedico <gedico3@gmail.com>
2026-09-26 17:36:33 +0200

    primo commit con collegamento al database

--- backend/inventory-manager/src/main/java/com/retrogamer/inventory_manager/InventoryManagerApplication.java
+++ backend/inventory-manager/src/main/java/com/retrogamer/inventory_manager/InventoryManagerApplication.java
@@ -1,13 +1,26 @@
 package com.retrogamer.inventory_manager;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class InventoryManagerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(InventoryManagerApplication.class, args);
-	}
+    static {
+        try {
+            Dotenv dotenv = Dotenv.configure()
+                    .ignoreIfMissing()
+                    .load();
+            dotenv.entries().forEach(entry -> {
+                System.setProperty(entry.getKey(), entry.getValue());
+            });
+        } catch (Exception e) {
+            System.out.println("⚠️ Nessun file .env trovato.");
+        }
+    }
 
+    public static void main(String[] args) {
+        SpringApplication.run(InventoryManagerApplication.class, args);
+    }
 }
